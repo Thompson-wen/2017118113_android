e183e4075fada61830861826f3ec566859c96ced
ThompsonWen <1803941375@qq.com>
2019-11-14 09:15:05 +0800

    召唤地图界面

--- HelloWorld/app/src/main/java/com/example/helloworld/HelloWorldActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -30,8 +30,8 @@ public class HelloWorldActivity extends AppCompatActivity {
         Hello2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:10086"));
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:"));
                 startActivity(intent);
             }
         });
