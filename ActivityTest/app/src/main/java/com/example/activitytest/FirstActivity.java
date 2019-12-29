5c60f0eb7b80f30e803ffedc564cac75dfb5dbd8
ThompsonWen <1803941375@qq.com>
2019-12-29 15:43:19 +0800

    第二、三次作业  三个活动的跳转

--- ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -9,6 +9,9 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
     }
